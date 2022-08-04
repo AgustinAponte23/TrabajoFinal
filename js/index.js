ef24e32b797daa2b101ffefd516a86d01c0158ea
AgustinAponte23 <aponteagustin1@gmail.com>
2022-08-04 16:29:36 -0300

    Primera entrega

--- js/index.js
+++ js/index.js
@@ -1,37 +1,59 @@
-const cursos = [
-    {
-       nombre: 'Marketing Digital',
-       descripcion: 'Con este curso podrás lograr un nivel avanzado. No se requieren conocimientos previos.',
-       precio: 2300 
-    },
-    {
-       nombre: 'Diseño grafico',
-       descripcion: 'Aprende a manejar las mejores herramientas gratuitas para diseño grafico',
-       precio: 3300 
-    },
-    {
-       nombre: 'Arte Digital',
-       descripcion: 'Conoce las mejores tecnicas del arte contemporaneo.',
-       precio: 5000 
-    }
-]
+const listaCursos = [];
 
-   function filtro() {
-    let busqueda = prompt('¿Qué curso quieres ver?');
-    if (busqueda == "marketing digital") { 
-        alert("Ya puedes ver la información del curso " + busqueda);
-        document.write("Curso: " + cursos[0].nombre + " Descripción: " + cursos[0].descripcion + " Precio: $" + cursos[0].precio);
-    } else if (busqueda == "diseño grafico"){
-        alert("Ya puedes ver la información del curso " + busqueda);
-        document.write("Curso: " + cursos[1].nombre + " Descripción: " + cursos[1].descripcion + " Precio: $" + cursos[1].precio);
-    } 
-    else if (busqueda == "arte digital"){
-        alert("Ya puedes ver la información del curso " + busqueda);
-        document.write("Curso: " + cursos[2].nombre + " Descripción: " + cursos[2].descripcion + " Precio: $" + cursos[2].precio);
+// Curso template y metodos
+class curso {
+    constructor(nombre, descripcion, precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio; 
     }
-   }
+
+    agregarCurso(){
+        listaCursos.push(cursoMD);
+    }
+
+    eliminarCurso(){
+        listaCursos.pop();
+    }
+
+    
+}
+// Creación del curso y push al array de ListaCursos
+const cursoMD = new curso('Marketing digital', 'El mejor curso de redes sociales para principiantes', 1500);
+cursoMD.agregarCurso();
+
+
+// Funciones
+function mostrarCurso() {
+    document.write(listaCursos[0].nombre , listaCursos[0].descripcion , listaCursos[0].precio)
+}
+
+function filtro() {
+    let busqueda = prompt('¿Qué curso quieres ver? (Número 1 para marketing digital)');
+    if (busqueda == 1) { 
+        mostrarCurso();
+    }
+}
 
 filtro();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Final de programa
 
 let repetir = confirm("¿Quieres ver algun curso mas?");
 
@@ -40,6 +62,8 @@ if (repetir) {
 } else {
     alert("Gracias por utilizar nuestra web");
 }
+// Se elimina el curso 
+cursoMD.eliminarCurso();
 
 
 
