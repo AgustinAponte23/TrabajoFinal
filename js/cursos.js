b4867815e74cf419206ac1102569f4d2a2e7a7d4
AgustinAponte23 <aponteagustin1@gmail.com>
2022-08-18 18:29:40 -0300

    Eventos y JSON

---
+++ js/cursos.js
@@ -0,0 +1,149 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Array Cursos
+    const cursos = [
+        {
+            id: 1,
+            nombre: 'CursoMarketing Digital',
+            precio: 2400,
+            imagen: 'marketing.jpg'
+        },
+        {
+            id: 2,
+            nombre: 'Curso Diseño grafico',
+            precio: 3200,
+            imagen: 'diseñografico.jpg'
+        },
+        {
+            id: 3,
+            nombre: 'Curso Arte Digital',
+            precio: 4000,
+            imagen: 'artedigital.jpg'
+        },
+
+    ];
+
+    //Variables
+    let carrito = [];
+    const divisa = '$';
+    const DOMitems = document.querySelector('#items');
+    const DOMcarrito = document.querySelector('#carrito');
+    const DOMtotal = document.querySelector('#total');
+    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+    const miLocalStorage = window.localStorage;
+
+    // Funciones
+    function renderizarProductos() {
+        cursos.forEach((info) => {
+            // Estructura
+            const miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-12');
+            // Body
+            const miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body');
+            // Titulo
+            const miNodoTitle = document.createElement('h5');
+            miNodoTitle.classList.add('card-title');
+            miNodoTitle.textContent = info.nombre;
+            // Imagen
+            const miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('img-fluid');
+            miNodoImagen.setAttribute('src', info.imagen);
+            // Precio
+            const miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.textContent = `${info.precio}${divisa}`;
+            // Boton 
+            const miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-success');
+            miNodoBoton.textContent = 'Agregar';
+            miNodoBoton.setAttribute('marcador', info.id);
+            miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+            // Insert
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.appendChild(miNodoTitle);
+            miNodoCardBody.appendChild(miNodoPrecio);
+            miNodoCardBody.appendChild(miNodoBoton);
+            miNodo.appendChild(miNodoCardBody);
+            DOMitems.appendChild(miNodo);
+        });
+    }
+
+    // Añadir curso a carrito
+    function anyadirProductoAlCarrito(evento) {
+        carrito.push(evento.target.getAttribute('marcador')) 
+        renderizarCarrito();
+        guardarCarritoEnLocalStorage();
+    }
+
+    // Cursos guardados en carrito
+    function renderizarCarrito() {
+        DOMcarrito.textContent = '';
+        const carritoSinDuplicados = [...new Set(carrito)];
+        carritoSinDuplicados.forEach((item) => {
+            const miItem = cursos.filter((itemCursos) => {
+                return itemCursos.id === parseInt(item);
+            });
+
+            // Cantidad
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            
+            // Item del carrito
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+
+            // Acción borrar
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.textContent = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        });
+        DOMtotal.textContent = calcularTotal();
+    }
+
+    // Vaciar
+    function borrarItemCarrito(evento) {
+        const id = evento.target.dataset.item;
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        renderizarCarrito();
+        guardarCarritoEnLocalStorage();
+
+    }
+
+    // Actualizar
+    function vaciarCarrito() {
+        carrito = [];
+        renderizarCarrito();
+        localStorage.clear();
+
+    }
+
+    function guardarCarritoEnLocalStorage () {
+        miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    function cargarCarritoDeLocalStorage () {
+        // ¿Existe un carrito previo guardado en LocalStorage?
+        if (miLocalStorage.getItem('carrito') !== null) {
+            // Carga la información
+            carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+        }
+    }
+
+    // Eventos
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+    // Inicio
+    cargarCarritoDeLocalStorage();
+    renderizarProductos();
+    renderizarCarrito();
+});
--- js/index.js
+++ js/index.js
@@ -1,61 +1,18 @@
-const listaCursos = [];
-
-// Curso template
-class Curso {
-    constructor(nombre, descripcion, precio) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio; 
-    }
-}
-
-// Creación del curso y push al array de ListaCursos
-const cursoMd = new Curso('Marketing digital', 'El mejor curso de redes sociales para principiantes', 1500);
-const cursoAd = new Curso('Arte digital', 'Conoce las mejores herramientas para el arte digital' ,3500);
-const cursoDg = new Curso('Diseño grafico', 'Aprende a diseñar las mejores graficas' ,5500);
-// Push
-listaCursos.push(cursoMd);
-listaCursos.push(cursoAd);
-listaCursos.push(cursoDg);
-
-
-// Funciones
-function mostrar() {
-    document.getElementById('buscador').style.display = 'block';
-}
-function eliminarCurso() {
-    listaCursos.pop();
-    for (const curso of listaCursos) {
-        console.log('Queda el curso: ' + curso.nombre);
-    }
-}
-// Eventos
-const eliminar = document.getElementById('eliminar');
-eliminar.onclick = eliminarCurso;
-
-
-// Control del buscador
 let form = document.getElementById('formulario');
 
-form.addEventListener('submit', function (e){
-    e.preventDefault(); 
-    let datos = new FormData(form);
-    let cursoBuscado = datos.get('nombre');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-    // Validación
-    if (cursoBuscado === "marketing digital") {
-        alert('Nombre: ' + listaCursos[0].nombre + ' Descripción:' + listaCursos[0].descripcion + ' Precio de: ' + ' $ ' + listaCursos[0].precio)
-    } else if(cursoBuscado === "arte digital") {
-        alert('Nombre: ' + listaCursos[1].nombre + ' Descripción:' + listaCursos[1].descripcion + ' Precio de: ' + ' $ ' + listaCursos[1].precio)
-    }else if(cursoBuscado === "diseño grafico"){
-        alert('Nombre: ' + listaCursos[2].nombre + ' Descripción:' + listaCursos[2].descripcion + ' Precio de: ' + ' $ ' + listaCursos[2].precio)
+    let data = new FormData(form);
+    let user = data.get('usuario')
+    let password = data.get('contra');
+    if (user === 'Admin' && password === 'admin123') {
+        alert('Bienvenido')
+    } else {
+        error();
     }
 })
 
-
-
-
-
-
-
-
+function error() {
+    document.getElementById('alerta').style.display = 'block';
+}
